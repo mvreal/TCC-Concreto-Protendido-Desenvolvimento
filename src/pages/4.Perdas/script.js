8e99ee0d66760bbce922008cbdf80b5434b589e7
Bruno Santos <bruno.if@hotmail.com>
2022-12-27 15:39:16 -0300

    Iniciando rotina das perdas

---
+++ src/pages/4.Perdas/script.js
@@ -0,0 +1,8 @@
+function inserirDadosSelect(dadosSalvosdaRotina3){
+    getSelect = document.getElementById('dadosEntrada')
+    dadosSalvosdaRotina3.forEach(element => {
+        let createOption = document.createElement('option')
+        getSelect.appendChild(createOption)
+        getSelect.innerHTML = 
+    });
+}
