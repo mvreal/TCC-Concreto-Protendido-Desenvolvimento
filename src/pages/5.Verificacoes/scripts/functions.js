f438b59915675a64e1b25e880fe73ce7450cfbf0
Bruno Santos <bruno.if@hotmail.com>
2023-02-13 22:06:06 -0300

    Criando a rotina 5

--- src/pages/5.Verificacoes/scripts/functions.js
+++ src/pages/5.Verificacoes/scripts/functions.js
@@ -17,24 +17,36 @@ function criaroption(arr){
 
 function calcularSigmac1(P0, Ac, ep, w1, Mg1){
     let sigmac1 = []
+
+    P0 = P0.map(el => el * 1000) // convertendo para N
+    Ac = Ac / 10000 // convertendo para m² 
+    w1 = w1 / 1000000 //convertendo para m³
+    Mg1 = Mg1.map(el => el * 1000) //convertendo para N * m 
+
     for(let i = 0; i < P0.length; i++){
-        sigmac1[i] = -1.1 * P0[i] * ((1 / Ac) + (ep[i] / w1)) - (Mg1 / w1)
+        sigmac1[i] = (-1.1 * P0[i] * ((1 / Ac) + (ep[i] / w1))) - (Mg1[i] / w1)
     }
-    return sigmac1
+    return sigmac1.map(el => el / 1000000)
 }
 
 function calcularSigmac2(P0, Ac, ep, w2, Mg1){
     let sigmac2 = []
+
+    P0 = P0.map(el => el * 1000) // convertendo para N
+    Ac = Ac / 10000 // convertendo para m² 
+    w2 = w2 / 1000000 //convertendo para m³
+    Mg1 = Mg1.map(el => el * 1000) //convertendo para N * m 
+    
     for(let i = 0; i < P0.length; i++){
-        sigmac2[i] = -1.1 * P0[i] * ((1 / Ac) + (ep[i] / w2)) - (Mg1 / w2)
+        sigmac2[i] = (-1.1 * P0[i] * ((1 / Ac) + (ep[i] / w2))) - (Mg1[i] / w2)
     }
+    return sigmac2.map(el => el / 1000000)
 }
 
 function pegarDadosRotina1(index){
     const dadosRotina1 = dadosSalvosdaRotina4[index]['dadosSalvosdaRotina3']['rotina2']['rotina1']
-
     return{
-        area: dadosRotina1['area'],
+        areaConcreto: dadosRotina1['area'],
         centroide: dadosRotina1['ixg'],
         w1: dadosRotina1['w1'],
         w2: dadosRotina1['w2'],
@@ -51,7 +63,7 @@ function pegarDadosRotina2(index){
         psi2: dadosRotina2['&#936<sub>2</sub>'],
         g1: dadosRotina2['g<sub>1</sub>'],
         g2: dadosRotina2['g<sub>2</sub>'],
-        q: dadosRotina2['q'],
+        q: dadosRotina2['q']
     }
 }
 
@@ -61,7 +73,9 @@ function pegarDadosRotina3(index){
     return{
         fck: dadosRotina3['fck'],
         Ap: dadosRotina3['Ap'],
-        ep: dadosRotina3['secoes'].map(el=>el['ep'])
+        ep: dadosRotina3['secoes'].map(el=>el['ep']),
+        vao: dadosRotina3['secoes'][0]['Vao'],
+        secoes: dadosRotina3['secoes'].map(el=>el['X'])
     }
 }
 
--- src/pages/5.Verificacoes/scripts/main.js
+++ src/pages/5.Verificacoes/scripts/main.js
@@ -1,21 +1,27 @@
-import { pegarDadosRotina4, pegarDadosRotina3, pegarDadosRotina2, pegarDadosRotina1, calcularMomentoFletor, criaroption } from "./functions.js"
+import { pegarDadosRotina4, pegarDadosRotina3, pegarDadosRotina2, pegarDadosRotina1, calcularMomentoFletor, criaroption, calcularSigmac1 } from "./functions.js"
 
 function main(){
     const select = document.getElementById('situacoes')
     const index = Number(select.value)
     const dado = dadosSalvosdaRotina4[index]
 
-    const {area, centroide, w1, w2, ixg, tipo} = pegarDadosRotina1(index)
+    const {areaConcreto, centroide, w1, w2, ixg, tipo} = pegarDadosRotina1(index)
     const {psi1, psi2, g1, g2, q} = pegarDadosRotina2(index)
-    const {fck, Ap, ep} = pegarDadosRotina3(index)
+    const {fck, Ap, ep, vao, secoes} = pegarDadosRotina3(index)
     const {perdaAtrito, perdaAncoragem, perdaEncurtamento, perdaFinal} = pegarDadosRotina4(index)
 
-    console.log(area, centroide, w1, w2, ixg, tipo)
+
+    console.log(areaConcreto, centroide, w1, w2, ixg, tipo)
     console.log(psi1, psi2, g1, g2, q)
     console.log(fck, Ap, ep)
     console.log(perdaAtrito, perdaAncoragem, perdaEncurtamento, perdaFinal)
+
+    const Mg1 = calcularMomentoFletor(g1, vao, secoes)
+    console.log(Mg1)
+
+    const sigmac1 = calcularSigmac1(perdaEncurtamento, areaConcreto, ep, w1, Mg1)
+    console.log(sigmac1)
+
 }
-
-
 
 export {main}
